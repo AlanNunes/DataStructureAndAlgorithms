f1a2869d90fdb7e17366a5b43e9db36fade40aa5
alannunes <alann.625@gmail.com>
2022-03-26 14:42:50 -0300

    Adicionar arquivos de projeto.

---
+++ Shared/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared.BinaryTree
+{
+    public static class BinarySearchTree
+    {
+        public static void Add(this Node root, Node node)
+        {
+            if (root.ExistLeftNode())
+            {
+                if ((int)node.Value <= (int)root.Left.Value)
+                    Add(root.Left, node);
+            }
+            if (root.ExistRightNode())
+            {
+                if ((int)node.Value > (int)root.Right.Value)
+                    Add(root.Right, node);
+            }
+        }
+    }
+}
---
+++ Shared/BinaryTree/Node.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.BinaryTree
+{
+    public class Node
+    {
+        public Node? Left { get; set; }
+        public object Value { get; set; }
+        public Node? Right { get; set; }
+
+        private Node() { }
+
+        public static Node Create(object value, Node? left = null, Node? right = null)
+            => new() { Left = left, Value = value, Right = right };
+
+        public bool ExistLeftNode()
+            => Left is not null;
+
+        public bool ExistRightNode()
+            => Right is not null;
+    }
+}
