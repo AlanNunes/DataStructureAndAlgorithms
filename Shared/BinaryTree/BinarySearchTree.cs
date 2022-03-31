5d6dfafe86f310b8c6f79107a73011bca7c63ba6
alannunes <alann.625@gmail.com>
2022-03-31 08:25:01 -0300

    Calc tree height

--- Shared/BinaryTree/BinarySearchTree.cs
+++ Shared/BinaryTree/BinarySearchTree.cs
@@ -4,14 +4,24 @@
     {
         public static void Add(this Node root, Node node)
         {
-            if (root.ExistLeftNode())
+            if (!root.ExistLeftNode())
             {
-                if ((int)node.Value <= (int)root.Left.Value)
+                if ((int)node.Value <= (int)root.Value)
+                    root.Left = node;
+            }
+            else
+            {
+                if ((int)node.Value <= (int)root.Value)
                     Add(root.Left, node);
             }
-            if (root.ExistRightNode())
+            if (!root.ExistRightNode())
             {
-                if ((int)node.Value > (int)root.Right.Value)
+                if ((int)node.Value > (int)root.Value)
+                    root.Right = node;
+            }
+            else
+            {
+                if ((int)node.Value > (int)root.Value)
                     Add(root.Right, node);
             }
         }
---
+++ TreeHeightCalc/Program.cs
@@ -0,0 +1,32 @@
+﻿using Shared.BinaryTree;
+
+var root = Node.Create(10);
+
+root.Add(Node.Create(13));
+root.Add(Node.Create(2));
+root.Add(Node.Create(7));
+root.Add(Node.Create(22));
+root.Add(Node.Create(23));
+root.Add(Node.Create(1));
+
+Console.WriteLine(CalcTreeHeight(root));
+
+int CalcTreeHeight(Node root)
+{
+    int countLeft = 0;
+    int countRight = 0;
+
+    if (root.ExistLeftNode())
+    {
+        countLeft = CalcTreeHeight(root.Left);
+    }
+    if (root.ExistRightNode())
+    {
+        countRight = CalcTreeHeight(root.Right);
+    }
+
+    if (countLeft > countRight)
+        return (countLeft + 1);
+    else
+        return (countRight + 1);
+}
